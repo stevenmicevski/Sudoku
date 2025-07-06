a4bc94317078b46b34508273decd6c464e9c78fb
153096 <stefan_micevski@ymail.com>
2025-07-06 16:43:40 +0200

    Insertion Logic added in the Sudoku project

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,16 @@
-board = [[0 for i in range(9)] for j in range (9)];
+#board = [[0 for i in range(9)] for j in range (9)];
+
+board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 0, 0]  # ← two missing numbers here
+]
 
 def print_board(board):
     for i in range(9):
@@ -11,4 +23,41 @@ def print_board(board):
         print("|");
     print("+-------+-------+-------+");
 
+
+def number_is_valid_in_row(board, number, row):
+    for i in board[row]:
+        if number==i:
+            return False;
+    return True;
+
+def number_is_valid_in_column(board, number, column):
+    for i in board:
+        if number==i[column]:
+            return False;
+    return True;
+
+def number_is_valid_in_box(board, number, row, column):
+    row = (row//3) * 3;
+    column = (column//3) * 3;
+    for row in range(row+2):
+        for column in range(column+2):
+            if number==board[row][column]:
+                return False;
+    return True;
+
+def insert_number(board, number, row, column):
+    valid_row = number_is_valid_in_row(board=board, number=number, row=row);
+    valid_column = number_is_valid_in_column(board=board, number=number, column=column);
+    valid_box = number_is_valid_in_box(board=board, number=number, row=row, column=column);
+
+
+    if valid_row and valid_column and valid_box:
+        board[row][column] = number;
+        print("Success! Well done.");
+    else:
+        print("Invalid, please try again.");
+
+
+print_board(board=board);
+insert_number(board, 7, 8, 7);
 print_board(board=board);
