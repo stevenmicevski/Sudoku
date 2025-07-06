2275094c78722a4312cdee13866de069f13c0dda
153096 <stefan_micevski@ymail.com>
2025-07-06 15:40:04 +0200

    Add print_board function for Sudoku grid display

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,14 @@
-print ("Hello");
+board = [[0 for i in range(9)] for j in range (9)];
+
+def print_board(board):
+    for i in range(9):
+        if i%3==0:
+            print("+-------+-------+-------+");
+        for j in range(9):
+            if j%3==0:
+                print("|", end=" ");
+            print (board[i][j], end=" "); 
+        print("|");
+    print("+-------+-------+-------+");
+
+print_board(board=board);
